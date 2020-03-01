c4ec9ffd361fb8f11f391aa3b011a8497a6d0310
jonas-bez <jonas.bezzerra@gmail.com>
2020-03-01 12:16:59 -0300

    foi adicionado melhorias nas funçoes e aautomatizaçao do codigo

--- tamagotchi_01.cpp
+++ tamagotchi_01.cpp
@@ -1,44 +1,38 @@
 #include <iostream>
 #include <stdlib.h>
+#include <sstream>
 using namespace std;
 
 struct Tamagotchi{
-    int felicidade = 0;
-    int comida = 100;
-    float inteligencia = 2;
-    int saude = 10;
-    //sono
+    int felicidade = 50;
+    int comida = 50;
+    int sono = 50;
+    int saude = 50;
 
-
+    //  A função brinca com o animal mas ele pode não brincar por estar com fome
+    //  +20 felicidade
+    //  -10 comida
+    //  +0.1 de inteligência
     bool brincar(){
-        if (this->comida < 30){
+ /*       if (this->comida < 10){
             cout<< "NÃO PODE BRINCAR ESTÁ COM MUITA FOME"<<endl;
             return false;
-        }
-
-        if(this->saude > 0){
-            cout << "Brincando com a bola\n";
-            if(this->felicidade>=80){
-                this->felicidade = 100; 
-            }
-            this->felicidade += 20;
-
-            if((this->comida -= 30)<0){
-                this->comida = 0;
-            }
-            this->comida -= 30;
-
-            this->inteligencia += 0.1;
-            return true;
-            }
-        
-        cout << "RIP\n";
-        exit(0);
+        }*/
+        cout << "Brincando com a bola\n";
+        this->felicidade>=80 ? this->felicidade = 100 : this->felicidade += 20;
+        this->comida <= 10 ? this->comida = 0 : this->comida -= 10;
+        this->sono += 15;
+        return true;            
     }
 
+
+    //  A função alimenta o animal mas ele pode morrer de gordo
+    //  +10 comida
+    //  pode ter -5 de saúde
     bool seAlimentar(){
-        if(this->comida > 100){
-            this->saude -= 5;
+        if(this->comida >= 91){
+            this->comida+=10;
+            this->saude-=5;
             cout<<"GORDO DEMAIS"<<endl;
             if(this->saude <=0){
                 cout<<"RIP"<<endl;
@@ -51,6 +45,9 @@ struct Tamagotchi{
         return true;
     }
 
+    // A função recupera saúde do animal
+    // +40 saúde
+    // -10 felicidade
     bool medico(){
         if(this->saude>=60){
             this->saude=100;
@@ -59,11 +56,34 @@ struct Tamagotchi{
             this->saude+=40;
             cout<< "ESTÁ SE CURANDO"<<endl;
         }
-        
-        if(this->felicidade<10){
-            this->felicidade=0; 
+
+        this->felicidade<10 ?  this->felicidade=0:this->felicidade-=10;
+        this->comida <20 ?  this->comida=0:this->comida-=20;
+
+        return true;
+    }
+
+    bool parametrosDeVida(){
+
+        if(this->felicidade==0){
+            this->saude -= 5;
+            this->comida -=5;
+            if(this->saude<=0){
+                cout<<"você e um pessimo dono matou o bicho"<<endl;
+                cout<<"RIP"<<endl;
+                exit(0);
+            }
         }
-        this->felicidade-=10;
+        if(this->comida==0){
+            this->saude-=10;
+            this->felicidade-=5;
+            if(this->saude<=0){
+                cout<<"você e um pessimo dono matou o bicho"<<endl;
+                cout<<"RIP"<<endl;
+                exit(0);
+            }           
+        }
+        return true;
     }
 
 };
@@ -71,18 +91,37 @@ struct Tamagotchi{
 int main() {
     Tamagotchi * tamagotchi = new Tamagotchi();
 
-    cout << "Felicidade " << tamagotchi->felicidade << "%"  <<endl;
-    cout << "Comida " << tamagotchi->comida << "%"  <<endl;
-    cout << "Inteligência " << tamagotchi->inteligencia  <<endl;
-    cout << "Saúde " << tamagotchi->saude << "%" <<endl;
+    while(true){
+        string line;
 
-    cout << "\n\n\n";
+        cout << "Felicidade " << tamagotchi->felicidade << "%"  <<endl;
+        cout << "Comida " << tamagotchi->comida << "%"  <<endl;
+        cout << "Inteligência " << tamagotchi->inteligencia  <<endl;
+        cout << "Saúde " << tamagotchi->saude << "%" <<endl;
+       
+        cout<<endl;
+        tamagotchi->parametrosDeVida();
+        cout<<endl;
 
-    tamagotchi->medico();
-    tamagotchi->seAlimentar();
-    tamagotchi->seAlimentar();
-    tamagotchi->seAlimentar();
-    tamagotchi->brincar();
+        getline(cin, line);
+        stringstream ss(line);
 
+
+
+        if(line=="exit"){
+            exit(0);
+        }else if(line=="medico"){
+            tamagotchi->medico();
+        }else if(line=="alimentar"){
+            tamagotchi->seAlimentar();
+        }else if(line=="brincar"){
+        tamagotchi->brincar();
+        }else{
+            cout<<"COMANDO INVÁLIDO"<<endl;
+        }
+
+        cout << endl;
+
+    }
     return 0;
 }
