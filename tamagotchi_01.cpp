55786f3ec9239951dcaaeb6cf5006382ea35d0fe
jonas-bez <jonas.bezzerra@gmail.com>
2020-03-01 15:53:28 -0300

    tamagotchi finalizado com menu e informaçoes

--- tamagotchi_01.cpp
+++ tamagotchi_01.cpp
@@ -12,12 +12,12 @@ struct Tamagotchi{
     //  A função brinca com o animal mas ele pode não brincar por estar com fome
     //  +20 felicidade
     //  -10 comida
-    //  +0.1 de inteligência
+    //  +15 de sono
     bool brincar(){
- /*       if (this->comida < 10){
+        if (this->comida <= 10){
             cout<< "NÃO PODE BRINCAR ESTÁ COM MUITA FOME"<<endl;
             return false;
-        }*/
+        }
         cout << "Brincando com a bola\n";
         this->felicidade>=80 ? this->felicidade = 100 : this->felicidade += 20;
         this->comida <= 10 ? this->comida = 0 : this->comida -= 10;
@@ -29,10 +29,13 @@ struct Tamagotchi{
     //  A função alimenta o animal mas ele pode morrer de gordo
     //  +10 comida
     //  pode ter -5 de saúde
+    //  +5 sono
     bool seAlimentar(){
         if(this->comida >= 91){
             this->comida+=10;
             this->saude-=5;
+            this->sono >= 90 ? this->sono = 100 : this->sono+=10 ;
+
             cout<<"GORDO DEMAIS"<<endl;
             if(this->saude <=0){
                 cout<<"RIP"<<endl;
@@ -42,29 +45,43 @@ struct Tamagotchi{
         }
         
         this->comida +=10;
+        this->sono >= 90 ? this->sono = 100 : this->sono+=10 ;
+
         return true;
     }
 
     // A função recupera saúde do animal
     // +40 saúde
-    // -10 felicidade
+    // -30 felicidade
+    // -30 comida
+
     bool medico(){
         if(this->saude>=60){
             this->saude=100;
         }
         else{
             this->saude+=40;
-            cout<< "ESTÁ SE CURANDO"<<endl;
+            cout<< "ESTÁ CURADO"<<endl;
         }
 
-        this->felicidade<10 ?  this->felicidade=0:this->felicidade-=10;
-        this->comida <20 ?  this->comida=0:this->comida-=20;
+        this->felicidade<30 ?  this->felicidade=0:this->felicidade-=30;
+        this->comida <30 ?  this->comida=0:this->comida-=30;
 
         return true;
     }
 
-    bool parametrosDeVida(){
+    //a função diminui o nono do animal
+    //-30 sono
+    //+10 felicidade
+    //-10 comida
+    bool dormir(){
+        this->sono <= 30 ? this->sono = 0 : this->sono-=30;
+        this->felicidade>90 ?  this->felicidade=100:this->felicidade+=10;
+        this->comida <10 ? this->comida=0:this->comida-=10;
+        return true;
+    }
 
+    bool parametrosDeVida(){
         if(this->felicidade==0){
             this->saude -= 5;
             this->comida -=5;
@@ -83,8 +100,32 @@ struct Tamagotchi{
                 exit(0);
             }           
         }
+
+        if(this->sono==100){
+            this->saude-=5;
+            this->felicidade-=5;
+            if(this->saude<=0){
+                cout<<"você e um pessimo dono matou o bicho"<<endl;
+                cout<<"RIP"<<endl;
+                exit(0);
+            }           
+        }
         return true;
     }
+
+    void informacoes(){
+        cout << "brincar : +20 felicidade, -10 comida, +15 sono "<<endl;
+        cout << "alimentar : +10 comida, 100 > -5 saúde, +10 sono "<<endl;
+        cout << "médico : +40 saúde, -30 felicidade, -30 comida "<<endl;
+        cout << "dormir : -30 sono, -10 comida, +10 felicidade "<<endl;
+        cout << "Se o tamagotchi estiver com muito sono, muito infeliz ou com muita fome, ele perde saúde e pode morrer"<<endl;
+
+    }
+
+    //  +10 comida
+    //  pode ter -5 de saúde
+    //  +5 sono
+
 
 };
 
@@ -94,10 +135,14 @@ int main() {
     while(true){
         string line;
 
-        cout << "Felicidade " << tamagotchi->felicidade << "%"  <<endl;
-        cout << "Comida " << tamagotchi->comida << "%"  <<endl;
-        cout << "Inteligência " << tamagotchi->inteligencia  <<endl;
-        cout << "Saúde " << tamagotchi->saude << "%" <<endl;
+        cout <<  "+-------------------------------------+" <<endl;
+        cout << "Felicidade " << tamagotchi->felicidade << "%" << "   - brincar" <<endl;
+        cout << "Comida " << tamagotchi->comida << "%" << "       - alimentar" <<endl;
+        cout << "Saúde " << tamagotchi->saude << "%" << "        - medico" <<endl;
+        cout << "Sono " << tamagotchi->sono << "%" << "         - dormir" <<endl;
+        cout <<  "                 - informacoes" <<endl;
+        cout <<  "                 - exit" <<endl;
+        cout <<  "+-------------------------------------+" <<endl;
        
         cout<<endl;
         tamagotchi->parametrosDeVida();
@@ -116,6 +161,10 @@ int main() {
             tamagotchi->seAlimentar();
         }else if(line=="brincar"){
         tamagotchi->brincar();
+        }else if(line=="dormir"){
+        tamagotchi->dormir();
+        }else if(line=="informacoes"){
+        tamagotchi->informacoes();
         }else{
             cout<<"COMANDO INVÁLIDO"<<endl;
         }
