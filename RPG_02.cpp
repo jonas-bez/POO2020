b45a139e2d6f9fc46f86eba323c41c3da2ed1eb1
jonas-bez <jonas.bezzerra@gmail.com>
2020-03-12 10:00:06 -0300

    atualizaçao de herança de classe

--- RPG_02.cpp
+++ RPG_02.cpp
@@ -1,23 +1,36 @@
-#include <iostream>
+#include <string>
+#include <sstream>
 #include <math.h>
+#include <iostream>
+#include <stdlib.h>
+//#include <bits\stdc++>
 
 using namespace std;
 
-struct Heroi{
 
+struct Mundo{
     string nome;
-    int hp = 12;
-    int atk = 3;
-    int def = 1;
-    int gold = 0;
+    int hp; 
+    int hp_tot; 
+    int atk;
+    int def;
+    int gold;
+};
 
-
+struct Heroi : Mundo{
     Heroi(){
+        string nome;
+        hp = 15;
+        hp_tot=15;
+        atk = 3;
+        def = 1;
+        gold = 0;
+
         cout<< "Un novo heroí está nascendo seu nome é:";
         cin >> this->nome;
         cout<<endl;
         cout<< "Nome: "<< this->nome<<endl;
-        cout<< "HP: "<< this->hp<<endl;
+        cout<< "HP: "<< this->hp <<"/ "<< this->hp_tot <<endl;
         cout<< "ATK: "<< this->atk<<endl;
         cout<< "DEF: "<< this->def<<endl;
         cout<< "GOLD: "<< this->gold<<endl;
@@ -30,19 +43,15 @@ struct Heroi{
     }
 };
 
-struct Monstro{
+struct Monstro : Mundo{
 
-    string nome;
-    int hp; 
-    int atk;
-    int def;
-    int gold;
 
     Monstro(bool qual_monstro ){
         if(qual_monstro==true){
             this->nome="Azazel";
             this->hp=10;
-            this->atk=2;
+            this->hp_tot=10;
+            this->atk=3;
             this->def=1;
             this->gold=4;
         }
@@ -50,7 +59,8 @@ struct Monstro{
         if(qual_monstro==false){
             this->nome="Slime";
             this->hp=8;
-            this->atk=1;
+            this->hp_tot=8;
+            this->atk=2;
             this->def=1;
             this->gold=2;
         }
@@ -63,44 +73,47 @@ struct Monstro{
 };
 
 
-
 int main() {
     Heroi * heroi = new Heroi; 
-    string menu;
+    string menu,decisao,time;
     bool rand=false;
 
-
     while (true)    {
-        cout<<"Bem vindo a midgard o que deseja fazer:"<<endl<<endl;
+        cin >> time;
+        system("clear");
+        cout<<"Bem vindo a midgard SR."<< heroi-> nome << " o que deseja fazer:"<<endl<<endl;
         cout<<"caçar"<<endl;
         cout<<"descansar"<<endl<<endl;
         cin >> menu;
         if(menu=="caçar"){
             rand = not(rand);
             Monstro * monstro = new Monstro(rand); 
-            
+            cout<<"lutar ou fugir"<<endl;            
             while (true){
-                cout<<"luta"<<endl;
-                cout<<heroi->nome<< " hp: "<<heroi->hp<<endl;
-                cout<<monstro->nome<< " hp: "<<monstro->hp<<endl;
-                heroi->hp -= fabs(monstro->atk-heroi->def);
-                monstro->hp -= fabs(heroi->atk-monstro->def);
-                if ( heroi->hp <= 0){
-                    delete heroi;
-                    return 0;
-                }else if(monstro->hp<=0){
-                    heroi->gold+=monstro->gold;
-                    delete monstro;
-
+                cout<<heroi->nome<< " HP: "<<heroi->hp <<"/ "<<heroi->hp_tot<<endl;
+                cout<<monstro->nome<< " HP: "<<monstro->hp<<"/ "<<monstro->hp_tot<<endl;
+                cin>>decisao;
+                if(decisao=="lutar"){
+                    heroi->hp -= fabs(monstro->atk-heroi->def);
+                    monstro->hp -= fabs(heroi->atk-monstro->def);
+                    if ( heroi->hp <= 0){
+                        delete heroi;
+                        return 0;
+                    }else if(monstro->hp<=0){
+                        heroi->gold+=monstro->gold;
+                        delete monstro;
+                        break;
+                    }
+                }else if(decisao == "fugir"){
                     break;
                 }
-
             }
         }else if(menu=="descansar"){
             cout<<"Recuperando vida"<<endl;
-            heroi->hp=12;
+            heroi->hp=15;
+
             cout<< "Nome: "<< heroi->nome<<endl;
-            cout<< "HP: "<< heroi->hp<<endl;
+            cout<< "HP: "<<heroi->hp <<"/ "<<heroi->hp_tot<<endl;
             cout<< "ATK: "<< heroi->atk<<endl;
             cout<< "DEF: "<< heroi->def<<endl;
             cout<< "GOLD: "<< heroi->gold<<endl;
@@ -109,6 +122,5 @@ int main() {
         }
     }
     
-
     return 0;
 }
